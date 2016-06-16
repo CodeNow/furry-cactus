72cc9e482c522c54dc264e65d174a9bc1bca0c60
Bryan Kendall <bryan@runnable.com>
2016-06-16 15:52:14 -0700

    refactoring and adding tests

---
+++ test/fixtures/swarm-info.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const swarmInfoGenerator = require('swarmerode/test/fixtures/swarm-info')
+
+const SWARM_INFO = swarmInfoGenerator([{
+  Labels: 'org=1000'
+}, {
+  Labels: 'org=2000'
+}])
+
+const reservedMemoryRegexp = /Reserved\ Memory/
+
+for (var i = 0; i < SWARM_INFO.SystemStatus.length; ++i) {
+  if (reservedMemoryRegexp.test(SWARM_INFO.SystemStatus[i][0])) {
+    SWARM_INFO.SystemStatus[i][1] = '4.000 GiB / 8.000 GiB'
+  }
+}
+
+console.log(SWARM_INFO)
+
+module.exports = SWARM_INFO
