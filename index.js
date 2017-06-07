3304478c49de72d0e888150d5566eadcd6ec6298
thejsj <jorge.silva@thejsj.com>
2017-06-06 18:03:46 -0700

    Add better logging. Remove all console.logs

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ const Promise = require('bluebird')
 
 const AWS = require('./lib/aws')
 const Swarm = require('./lib/swarm')
+const logger = require('./loggers').child({ module: 'models/main' })
+const log = logger.child({ method: 'main' })
+
 
 const swarm = new Swarm()
 
+log.info('Start')
 Promise.props({
   docks: AWS.getDocks(),
   swarmHosts: swarm.getInfo()
 })
-  .tap((data) => (AWS.sendBasicInfoToCloudWatch(data.docks, data.swarmHosts)))
-  .tap((data) => (AWS.sendThresholdDataToCloudWatch(data.swarmHosts)))
-  .tap((data) => (AWS.sendMaximumAvailableToCloudWatch(data.swarmHosts)))
+  .tap(data => log.trace({ data }, 'Done fetching docks and swarm hots'))
+  .tap(data => (AWS.sendBasicInfoToCloudWatch(data.docks, data.swarmHosts)))
+  .tap(data => log.trace({ data }, 'Done sending basic info to cloudwatch'))
+  .tap(data => (AWS.sendThresholdDataToCloudWatch(data.swarmHosts)))
+  .tap(data => log.trace({ data }, 'Done sending threshold data to cloudwatch'))
+  .tap(data => (AWS.sendMaximumAvailableToCloudWatch(data.swarmHosts)))
+  .tap(data => log.trace({ data }, 'Done sending max available to cloudwatch'))
   .catch((err) => {
-    console.log(err.stack || err.message || err)
+    log.error({ err }, 'Error running script')
     process.exit(1)
   })
